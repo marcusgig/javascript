a4205b52b431187169da402772cee61ca4aaa091
marcusgig <marcusviniciusaf.dev@gmail.com>
2025-12-08 01:26:38 -0300

    Ultimas alteracoes no randomizador e insercao da parte 3 das aulas

--- Javacript pt2/script.js
+++ Javacript pt2/script.js
@@ -1,10 +1,18 @@
-function generateNumber(){
+function generateNumber() {
     console.log("clicou");
 
-const min = Math.ceil(document.querySelector(".input-min").value);
-const max = Math.floor(document.querySelector(".input-max").value);
-const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    const min = Math.ceil(document.querySelector(".input-min").value);
+    const max = Math.floor(document.querySelector(".input-max").value);
 
-alert(`O número sorteado foi: ${result}`);
+
+    if (min >= max) {
+        alert("O valor minimo deve ser menor que o maximo");
+
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+        alert(`O número sorteado foi: ${result}`);
+    }
+
+    
 
 }
